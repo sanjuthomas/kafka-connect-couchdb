217dc2fdceed4276638ec3e7ceae2dc92c87ec30
Thomas <sathomas@ct1-8780-m.corp.pcln.com>
2018-02-13 17:04:40 -0500

    adding the .gitignore

---
+++ src/main/java/kafka/connect/kafka_connect_couchdb/App.java
@@ -0,0 +1,13 @@
+package kafka.connect.kafka_connect_couchdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
